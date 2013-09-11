75128416ffa87896877f521355f167dca93b9802
rahul <rahul.baxi@gmail.com>
2013-09-10 23:34:32 -0700

    display menu items

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Backstage::Application.routes.draw do
     resources :menu_items
   end
   
-  resources :menu_items
-  
   root :to => 'dashboard#home'
   match '/assign' => 'tablets#assign'
   match '/assign_tablets' => 'tablets#assign_tablets'
